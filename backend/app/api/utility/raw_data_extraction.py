c63db0272d21f5b782484b636ca0f4efa61362f8
farzamababafi <farzam.ababafi123@gmail.com>
2026-03-04 16:10:19 +0000

    code cleaned  commit.

--- backend/app/api/utility/raw_data_extraction.py
+++ backend/app/api/utility/raw_data_extraction.py
@@ -18,15 +18,19 @@ def raw_data_extraction(stock_name: str , start_date:str=None , end_date:str=Non
     
     start = pd.to_datetime(start_date)
     end = pd.to_datetime(end_date)
-
+    
+    df = pd.read_csv(file_name)
+    
     min_date = df["Date"].min()
     max_date = df["Date"].max()
+    print("----------------------------------------------------")
+    print((type(min_date), type(start_date)))
 
-    if start < min_date and end > max_date:
+    if start_date < min_date and end_date > max_date:
         raise ValueError(f"Date out of range. Available range: {min_date.date()} to {max_date.date()}")
          
 
-    df = pd.read_csv(file_name)
+
     
     df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
 
